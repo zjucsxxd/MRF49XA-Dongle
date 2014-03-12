925d6f45394ca4fc7cfb3bced2c1d0bb43efd992
William Dillon <hpux735@gmail.com>
2014-03-11 20:09:53 -0700

    Set explicit values for modes, necessary for consistent usage of the EEPROM

---
+++ modes.h
@@ -0,0 +1,26 @@
+//
+//  modes.h
+//  MRF49XA-Dongle
+//
+//  Created by William Dillon on 2/11/14.
+//  Copyright (c) 2014 Oregon State University (COAS). All rights reserved.
+//
+
+#ifndef MRF49XA_Dongle_modes_h
+#define MRF49XA_Dongle_modes_h
+
+enum device_mode {
+    INVALID    = 0,
+    SERIAL     = 1,
+    SERIAL_ECC = 2,
+    PACKET     = 3,
+    PACKET_ECC = 4,
+    MENU       = 5,
+    CAPTURE    = 6,
+    TEST_ALT   = 7,
+    TEST_ZERO  = 8,
+    TEST_ONE   = 9,
+    TEST_PING  = 10
+};
+
+#endif
